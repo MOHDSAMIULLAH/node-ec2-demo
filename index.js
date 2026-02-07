266ebeb09f0e660e284190b176f01208a0793caf
MOHD SAMIULLAH <samiullah0813@gmail.com>
2026-02-07 17:33:34 +0530

    api endpoint

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('EC2 deployment successful!');
 });
+
+app.get("/api", (req, res) => {
+    const data = {
+        message: "Hello from the API!", 
+        timestamp: new Date()    };
+    res.json(data);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
